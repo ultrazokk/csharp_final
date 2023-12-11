c66191a6e885bd3a68b34b2829c06130a5b6a9f3
Andrey Savaskov <ultrazokk@gmail.com>
2023-12-11 17:35:40 +0300

    Выполнение первого задания

---
+++ 1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+
+
+
+
+class Program
+{
+    static void PrintNumbersInRange(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNumbersInRange(m + 1, n);
+        }
+    }
+
+    static void Main()
+    {
+        int m = 1; 
+        int n = 5; 
+
+        Console.WriteLine($"Натуральные числа от {m} до {n}:");
+        PrintNumbersInRange(m, n);
+    }
+}
+
+
+
+
+
+
